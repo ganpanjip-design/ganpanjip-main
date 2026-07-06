fe2173c11accc481545d3adeef7e8519effc85df
comsieun <ja24z8@naver.com>
2026-07-06 21:59:26 +0900

    thumbnail 지정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,6 +76,7 @@ const HomePage: NextPage<Props> = ({ works: worksData }) => {
       <Head>
         <title>GANPANJIP</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="/logo.png" />
       </Head>
       <Header onMenuClick={() => setIsMenuOpen(true)} />
 
